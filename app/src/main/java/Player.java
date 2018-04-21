47601dd04f9888a2b28194ed93ea3e175832f0cd
Konatsup <konatsukomatsu@gmail.com>
2018-04-21 19:21:01 +0900

    Initial commit

---
+++ app/src/main/java/Player.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Owner on 2018/04/21.
+ */
+
+public class Player {
+    int ID;
+    int HP;
+    int skillID;
+    Player(int ID,int HP,int skillID) {
+        this.ID = ID;
+        this.HP = HP;
+        this.skillID = skillID;
+    }
+}
